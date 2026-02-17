3b3e6c2344c30a875040555fb5116381ae4d987c
Kawaki1963 <kawaki12353@gmail.com>
2026-02-17 11:32:19 -0300

    Criar trainer-data.js

---
+++ trainer-data.js
@@ -0,0 +1,44 @@
+const trainers = [
+{
+  trainer: "Captain Selene",
+  mode: "1v1",
+  photo: "assets/trainers/captain_selene.png",
+  description_pt: "*Cadetes! Mantenham a formação. ...Ah, você aí. Você não é um dos meus recrutas.*",
+  description_en: "*Cadets! Hold formation. ...Ah, you there. You're not one is my recruits.*",
+  drops_pt: "Marine Crate, 600 Credits",
+  drops_en: "Marine Crate, 600 Credits",
+  animons: ["Cadet", "Kid Hero", "Axe Man", "Taesheegi"]
+},
+{
+  trainer: "Manager Vira",
+  mode: "1v1",
+  photo: "assets/trainers/manager_vira.png",
+  description_pt: "Por enquanto nada...",
+  description_en: "Nothing for now....",
+  drops_pt: "Highstar Crate, ??? Credits",
+  drops_en: "Highstar Crate, ??? Credits",
+  animons: ["???"]
+},
+{
+  trainer: "Bandit King Lucas",
+  mode: "2v2",
+  photo: "assets/trainers/bandit_king_lucas.png",
+  description_pt: "*Eu sou o Rei desta Floresta! O Rei Bandido Lucas, para ser mais exato!*",
+  description_en: "*I'm the King of this Forest! Bandit King Lucas, to be exact!*",
+  drops_pt: "Bandit Crate, 600 Credits",
+  drops_en: "Bandit Crate, 600 Credits",
+  animons: ["Bandit", "Strong Bandit", "Bandit", "Battle Wolf", "Teen Wolfman"]
+}
+];
+
+const animonElements = {
+  "Cadet": ["neutral"],
+  "Kid Hero": ["water"],
+  "Axe Man": ["earth"],
+  "Taesheegi": ["water"],
+  "Bandit": ["neutral"],
+  "Strong Bandit": ["dark"],
+  "Battle Wolf": ["neutral"],
+  "Teen Wolfman": ["fighting"],
+  "???": []
+};
