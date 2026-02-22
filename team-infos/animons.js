141bbae470a2bf1acfcf5f5eabe19f21b85cdda4
Kawaki1963 <kawaki12353@gmail.com>
2026-02-21 23:59:26 -0300

    Add animons data to animons.js

---
+++ team-infos/animons.js
@@ -0,0 +1,5 @@
+const animons = [      
+ {id:1,name:"Ruffy",type:"Fighting",sprite:"assets/animons-icon/ruffy.png",stats:{hp:46,atk:65,def:55,spAtk:35,spDef:44,spd:55}},
+ {id:1.01,name:"TS Ruffy",type:"Fighting",sprite:"assets/animons-icon/ts_ruffy.png",stats:{hp:62,atk:93,def:70,spAtk:25,spDef:52,spd:75}},   
+ {id:2,name:"Mystic Bojin",type:"Psychic",sprite:"assets/animons-icon/mystic_bojin.png",stats:{hp:62,atk:33,def:52,spAtk:70,spDef:46,spd:45}}          
+];    
