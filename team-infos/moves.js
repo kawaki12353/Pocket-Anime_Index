efbafd15a63eb9c8bd660925b164c26381cad70d
Kawaki1963 <kawaki12353@gmail.com>
2026-02-22 00:17:43 -0300

    Atualizar o moves.js

--- team-infos/moves.js
+++ team-infos/moves.js
@@ -1,22 +1,62 @@
 const moves = [
   {
-    name: "Fire Blast",
-    type: "Fire",
-    power: 90,
-  },
-  {
-    name: "Scratch",
+    name: "Rubber Balloon",
     type: "Neutral",
+    attack: "Status",
     power: 40,
   },
   {
-    name: "Water Gun",
-    type: "Water",
+    name: "Punch",
+    type: "Neutral",
+    attack: "Physical",
+    power: 40,
+  },
+  {
+    name: "Rapid Punches",
+    type: "Fighting",
+    attack: "Physical",
+    power: 40,
+  },
+  {
+    name: "Rubber Pistol",
+    type: "Fighting",
+    attack: "Physical",
+    power: 40,
+  },
+  {
+    name: "Fighting Spirit",
+    type: "Fighting",
+    attack: "Status",
+    power: 40,
+  },
+  {
+    name: "Bazooka",
+    type: "Fighting",
+    attack: "Physical",
+    power: 40,
+  },
+  {
+    name: "Phase Two",
+    type: "Neutral",
+    attack: "Status",
+    power: -,
+  },
+  {
+    name: "Headbutt",
+    type: "Neutral",
+    attack: "Physical",
+    power: 40,
+  },
+  {
+    name: "Armament",
+    type: "Fighting",
+    attack: "Status",
     power: 40,
   },
   {
     name: "Thunder Shock",
     type: "Lightning",
+    attack: "Special",
     power: 40,
   }
 ];
