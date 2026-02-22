7b505d79ebd0120a8170abfb8a775579a575389f
Kawaki1963 <kawaki12353@gmail.com>
2026-02-22 00:03:22 -0300

    Criar moves.js

---
+++ team-infos/moves.js
@@ -0,0 +1,26 @@
+const moves = [
+  {
+    name: "Fire Blast",
+    type: "Fogo",
+    power: 90,
+    description: "Um ataque de fogo potente"
+  },
+  {
+    name: "Scratch",
+    type: "Normal",
+    power: 40,
+    description: "Arranhão básico"
+  },
+  {
+    name: "Water Gun",
+    type: "Água",
+    power: 40,
+    description: "Jato de água"
+  },
+  {
+    name: "Thunder Shock",
+    type: "Elétrico",
+    power: 40,
+    description: "Choque elétrico"
+  }
+];
