37e84ba03ffa38cffc662280dc75b0a2a69ce4d5
Kawaki1963 <kawaki12353@gmail.com>
2026-02-22 19:24:27 -0300

    Atualizar o animons.js

--- team-infos/animons.js
+++ team-infos/animons.js
@@ -1,6 +1,6 @@
 var animons = [      
- {id:1,name:"Ruffy",type:"Fighting",sprite:"assets/animons-icon/ruffy.png",stats:{hp:46,atk:65,def:55,spAtk:35,spDef:44,spd:55},movesList: ["Punch", "Rapid Punches", "Wind Slash"]},
- {id:1.01,name:"TS Ruffy",type:"Fighting",sprite:"assets/animons-icon/ts_ruffy.png",stats:{hp:62,atk:93,def:70,spAtk:25,spDef:52,spd:75}},   
+ {id:1,name:"Ruffy",type:"Fighting",sprite:"assets/animons-icon/ruffy.png",stats:{hp:46,atk:65,def:55,spAtk:35,spDef:44,spd:55},movesList: ["Punch", "Rubber Pistol", "Rapid Punches", "Fighting Spirit", "Bazooka", "Phase Two", "Headbutt", "Armament"]},
+ {id:1.01,name:"TS Ruffy",type:"Fighting",sprite:"assets/animons-icon/ts_ruffy.png",stats:{hp:62,atk:93,def:70,spAtk:25,spDef:52,spd:75},movesList: ["Punch", "Rubber Pistol", "Rapid Punches", "Fighting Spirit", "Bazooka", "Phase Two", "Headbutt", "Armament"]},   
  {id:2,name:"Mystic Bojin",type:"Psychic",sprite:"assets/animons-icon/mystic_bojin.png",stats:{hp:62,atk:33,def:52,spAtk:70,spDef:46,spd:45}},
  {id:2.01,name:"Super Bojin",type:"Psychic",sprite:"assets/animons-icon/super_bojin.png",stats:{hp:80,atk:39,def:67,spAtk:90,spDef:49,spd:63}}
 ];    
