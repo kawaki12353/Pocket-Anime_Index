07fdebb7ec5895ba30d0e8d9a157a96ef1f3d103
Kawaki1963 <kawaki12353@gmail.com>
2026-02-23 15:45:59 -0300

    Atualizar o animons.js

--- team-infos/animons.js
+++ team-infos/animons.js
@@ -5,6 +5,6 @@ var animons = [
  {id:2.01,name:"Super Bojin",type:"Psychic",sprite:"assets/animons-icon/super_bojin.png",stats:{hp:80,atk:39,def:67,spAtk:90,spDef:49,spd:63}},
  {id:3,name:"Sasken",type:"Fire",sprite:"assets/animons-icon/sasken.png",stats:{hp:40,atk:52,def:43,spAtk:60,spDef:45,spd:60},movesList: ["Punch", "Rubber Pistol", "Rapid Punches", "Fighting Spirit", "Bazooka", "Phase Two", "Headbutt", "Armament"]},
  {id:3.01,name:"TS Saskeh",type:["Fire", "Lightning"],sprite:"assets/animons-icon/ts_saskeh.png",stats:{hp:58,atk:75,def:53,spAtk:87,spDef:55,spd:70},movesList: ["Punch", "Rubber Pistol", "Rapid Punches", "Fighting Spirit", "Bazooka", "Phase Two", "Headbutt", "Armament"]},
-{id:4,name:"Isamu",type:"Neutral",sprite:"assets/animons-icon/isamu.png",stats:{hp:45,atk:54,def:35,spAtk:50,spDef:45,spd:71}},
+  {id:4,name:"Isamu",type:"Neutral",sprite:"assets/animons-icon/isamu.png",stats:{hp:45,atk:54,def:35,spAtk:50,spDef:45,spd:71}},
  {id:4.01,name:"Release Isamu",type:["Dark", "Soul"],sprite:"assets/animons-icon/release_isamu.png",stats:{hp:58,atk:87,def:52,spAtk:80,spDef:47,spd:97}}
 ];    
