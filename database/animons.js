e0c52b49331454e5a09b5230952f68b5cd56ea00
Kawaki1963 <kawaki12353@gmail.com>
2026-03-01 18:03:03 -0300

    Atualizar o animons.js

--- database/animons.js
+++ database/animons.js
@@ -70,7 +70,6 @@ const animons = [
       { level: 13, name: "Electric Strike" },
       { level: 15, name: "Vangeful Illusion" },
       { level: 18, name: "Pheonix Fire Bomb" },
-      { level: 21, name: "Electric Strike Spear" },
       { level: 24, name: "Heavenly Flames" }
     ]
   },
