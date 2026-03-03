a4d2e3f180840ccdebef99a872d9687e6cdb6cde
Kyodaina0 <159590637+Kyodaina0@users.noreply.github.com>
2026-03-03 20:55:20 -0300

    8

--- database/animons.js
+++ database/animons.js
@@ -179,7 +179,16 @@ const animons = [
       { level: 24, name: "Armament" }
     ]
   },
-  { id: 8, name: "Battle Wolf", type: "Neutral", sprite: "assets/animons-icon/battle_wolf.png", stats: { hp: 35, atk: 62, def: 33, spAtk: 25, spDef: 28, spd: 70 } },
+  { id: 8, name: "Battle Wolf", type: "Neutral", sprite: "assets/animons-icon/battle_wolf.png", stats: { hp: 35, atk: 62, def: 33, spAtk: 25, spDef: 28, spd: 70 },
+     movesList: [
+      { level: 1, name: "Growl"}, 
+      { level: 1, name: "Sharp Claw" },
+      { level: 5, name: "Quick Attack" },
+      { level: 11, name: "Bite" },
+      { level: 17, name: "Savage Fang" },
+      { level: 23, name: "Ripping Claw" }
+   ]
+  },
   { id: 8.01, name: "Teen Battle Wolf", type: "Beast", sprite: "assets/animons-icon/teen_battle_wolf.png", stats: { hp: 54, atk: 85, def: 44, spAtk: 36, spDef: 40, spd: 90 } },
   { id: 9, name: "Joyful", type: ["Wind", "Beast"], sprite: "assets/animons-icon/joyful.png", stats: { hp: 35, atk: 25, def: 35, spAtk: 50, spDef: 35, spd: 70 } },
   { id: 10, name: "Teen Wolfman", type: "Fighting", sprite: "assets/animons-icon/teen_wolfman.png", stats: { hp: 35, atk: 53, def: 35, spAtk: 49, spDef: 30, spd: 51 } },
