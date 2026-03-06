ef726456f6000b1b5ec7156a944f4439cbe7e424
Kawaki1963 <kawaki12353@gmail.com>
2026-03-06 16:22:39 -0300

    Atualizar o traits.js

--- database/traits.js
+++ database/traits.js
@@ -1,4 +1,32 @@
 const traits = {
+    "Sadist": {
+        desc_pt: "Causa 1.2x de dano ao atacar um alvo com uma condição de status.",
+        desc_en: "Deal 1.2x damage when attacking a target with a status condition."
+    },
+    "Fearsome": {
+        desc_pt: "Diminui o [001]ATK [-1][000] de todos os inimigos ao entrar em batalha.",
+        desc_en: "Decrease the [001]ATK [-1][000] of all enemies on summon."
+    },
+    "Major Threat": {
+        desc_pt: "Inimigos gastam o dobro de PP ao usar movimentos que têm o usuário como alvo.",
+        desc_en: "Enemies spend twice as much PP when using moves that target the user."
+    },
+    "Legendary Warrior": {
+        desc_pt: "Turnos consecutivos acertando ataques aumentam [002]ATK [+1][000] ou [002]SP.ATK [+1][000], até um máximo de +3. Contra animons do atributo Saijan aumenta +2 por turno.",
+        desc_en: "Consecutive turns landing attacks increase [002]ATK [+1][000] or [002]SP.ATK [+1][000], up to a maximum of +3. This increases to +2 per turn against Saijan-attribute animon."
+    },
+    "Fox Spirit": {
+        desc_pt: "Quando a vida estiver abaixo de 40%, entre em fúria, ganhe [002]ATQ [+2][000], [002]SP.ATK [+2][000] e [002]SPD [+2][000] e você estará em fúria. Após a transformação, recupere 10% da vida por 3 turnos.",
+        desc_en: "When below 40% HP, go berserk, gain [002]ATK [+2][000], [002]SP.ATK [+2][000] and [002]SPD [+2][000], and you are berserk. After transforming, heal 10% HP for 3 turns."
+    },
+    "Ninjutsu Mastery": {
+        desc_pt: "Usar habilidades do atributo chakra causa 1.2x mais dano.",
+        desc_en: "Using chakra-attribute skills will have an increased 1.2x damage."
+    },
+    "Wind Affinity": {
+        desc_pt: "Ao usar um movimento do tipo Wind, há 25% de chance de aumentar o dano em 1.2x.",
+        desc_en: "When using a Wind-Type move have a 25% chance to increase its damage 1.2x."
+    },
     "Consume": {
         desc_pt: "Após derrotar um inimigo, você recupera 30% do seu HP máximo.",
         desc_en: "After defeating an enemy restore 30% of your maximum HP."
