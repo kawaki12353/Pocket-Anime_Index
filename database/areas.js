b250a11319b94296e7ad6dd1c10125a654a63e5c
Kawaki1963 <kawaki12353@gmail.com>
2026-03-06 17:25:43 -0300

    Criar areas.js

---
+++ database/areas.js
@@ -0,0 +1,8 @@
+const areas = {
+    "0006": {
+        file: "assets/areas/0006.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área do Bandit King",
+        desc_en: "Bandit King Area"
+    }
+}
