2f0e2966a615602aba6c51dda03116f7a9cc68fd
Kawaki1963 <kawaki12353@gmail.com>
2026-03-06 12:27:43 -0300

    Criar traits.js

---
+++ database/traits.js
@@ -0,0 +1,12 @@
+const traits = {
+    "Consume": {
+        chance: "20%",
+        desc_pt: "Após derrotar um inimigo, você recupera 30% do seu HP máximo.",
+        desc_en: "After defeating an enemy restore 30% of your maximum HP."
+    },
+    "Fast Step": {
+        chance: "20%",
+        desc_pt: "Ganhe [002]SPD[+1][000] ao entrar em batalha contra inimigos mais rápidos.",
+        desc_en: "Gain [002]SPD[+1][000] when entering battle against faster enemies."
+    }
+};
