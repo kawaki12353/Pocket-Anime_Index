b424695446ac20377162dcc40260474fe2c516e5
Kawaki1963 <kawaki12353@gmail.com>
2026-03-06 12:34:08 -0300

    Atualizar o traits.js

--- database/traits.js
+++ database/traits.js
@@ -8,5 +8,25 @@ const traits = {
         chance: "20%",
         desc_pt: "Ganhe [002]SPD[+1][000] ao entrar em batalha contra inimigos mais rápidos.",
         desc_en: "Gain [002]SPD[+1][000] when entering battle against faster enemies."
+    },
+    "Copycat": {
+        chance: "20%",
+        desc_pt: "Usar o mesmo golpe que um alvo usou em você faz com que cause 25% a mais de dano.",
+        desc_en: "Using the same move a target has used on you makes it deal 25% more damage."
+    },
+    "Confident": {
+        chance: "100%",
+        desc_pt: "Quando um oponente tiver um de seus status reduzido, aumente seu [002]SPD[+1][000].",
+        desc_en: "When an opponent has one of their stats lowered, increase your [002]SPD[+1][002]."
+    },
+    "Perfect": {
+        chance: "5%",
+        desc_pt: "Se não for atingido por um golpe super efetivo, cure 1/12 do HP naquele turno. Se for atingido por um golpe físico, ganhe [002]SPD[+1][000] uma vez por batalha.",
+        desc_en: "If not hit by a super-effective move, heal 1/12 that turn. If hit by a physical move, gain [002]SPD[+1][002] once per battle."
+    },
+    "Reincarnation": {
+        chance: "10%",
+        desc_pt: "Se estiver afetado por uma condição de status ao final do turno, há 1/4 de chance de recuperar 1/12 do HP e remover todas as condições de status.",
+        desc_en: "If afflicted by a status condition at the end of a turn, there is a 1/4 chance to recover 1/12th HP and remove all status conditions."
     }
 };
