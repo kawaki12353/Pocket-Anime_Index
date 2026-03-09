1553c11758a11adbc3a759ccdbf3c6a1828411b5
Kawaki1963 <kawaki12353@gmail.com>
2026-03-09 16:44:31 -0300

    Atualizar o areas.js

--- database/areas.js
+++ database/areas.js
@@ -5,6 +5,36 @@ const areas = {
         desc_pt: "Área do Bandit King",
         desc_en: "Bandit King Area"
     },
+    "0005": {
+        file: "assets/areas/0005.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área do Treinador Molly",
+        desc_en: "Trainer Molly's Patch"
+    },
+    "0004": {
+        file: "assets/areas/0004.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área do Escalador",
+        desc_en: "Climber Patch"
+    },
+    "0003": {
+        file: "assets/areas/0003.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área do Bandit Drake",
+        desc_en: "Bandit Drake Patch"
+    },
+    "0002": {
+        file: "assets/areas/0002.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área da Cadet Mia",
+        desc_en: "Cadet Mia Patch"
+    },
+    "0001": {
+        file: "assets/areas/0001.png",
+        region: "Sunburst Forest",
+        desc_pt: "Área Inicial",
+        desc_en: "Starter Patch"
+    },
     "0000": {
         file: "assets/areas/0000.png",
         region: "Sunburst Valley",
