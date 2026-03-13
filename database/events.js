e1eb8cfc2904b1fa85f8b36b8030b75054ec606f
Kawaki1963 <kawaki12353@gmail.com>
2026-03-13 10:19:15 -0300

    Atualizar o events.js

--- database/events.js
+++ database/events.js
@@ -2,8 +2,8 @@ const events = {
   "Shinobi": {
     name: "Shinobi",
     chat: "Shinobi are arriving infront of the Forest of Doom!",
-    desc_pt: "Aumenta a chance de aparecer o Ramen na Área do Bandit King.",
-    desc_en: "It increases the chance of Ramen appearing in the Bandit King Patch.",
+    desc_pt: "Aumenta a chance de aparecer o [002]Ramen[000] na Área do Bandit King.",
+    desc_en: "It increases the chance of [002]Ramen[000] appearing in the Bandit King Patch.",
     sprite: "assets/events/shinobi.png",
     animon: ["Ramen"],
   },
