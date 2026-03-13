af37da59ea4101addbde5efdbdf57dcc28122ed1
Kawaki1963 <kawaki12353@gmail.com>
2026-03-12 21:03:28 -0300

    Criar events.js

---
+++ database/events.js
@@ -0,0 +1,12 @@
+const events = {
+  "Lightning Spread": {
+    name: "Lightning Spread",
+    desc_pt: "Libera um choque elétrico que ignora precisão e esquiva.",
+    desc_en: "Release an electric shock that ignores accuracy and evasion.",
+    type: "lightning",
+    attack: "special",
+    power: 65,
+    accuracy: 100,
+    pp: 15,
+  },
+}
