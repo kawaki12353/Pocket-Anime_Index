087cf0b5a4893a1eac32b21812394e18f7dcd8b7
Kawaki1963 <kawaki12353@gmail.com>
2026-03-12 21:09:36 -0300

    Atualizar o events.js

--- database/events.js
+++ database/events.js
@@ -5,6 +5,6 @@ const events = {
     desc_pt: "Aumenta a chance de aparecer o Ramen na Área do Bandit King.",
     desc_en: "It increases the chance of Ramen appearing in the Bandit King Patch.",
     sprite: "assets/events/shinobi.png",
-    animon: 65,
+    animon: "Ramen",
   },
 };
