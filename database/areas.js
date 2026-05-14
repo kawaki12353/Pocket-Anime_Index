6b930786a6b29591cfe010e97b7fcb4441bae02b
Kawaki1963 <kawaki12353@gmail.com>
2026-05-14 15:41:31 -0300

    Atualizar o areas.js

--- database/areas.js
+++ database/areas.js
@@ -1,4 +1,112 @@
 const areas = {
+    "0027": {
+        file: "assets/areas/0027.png",
+        region: "Great Plains",
+        desc_pt: "Base da Highstar",
+        desc_en: "Highstar Base Area"
+    },
+    "0026": {
+        file: "assets/areas/0026.png",
+        region: "Great Plains",
+        desc_pt: "Área da Caveira de Macaco",
+        desc_en: "Monkey Skull Patch"
+    },
+    "0025": {
+        file: "assets/areas/0025.png",
+        region: "Great Plains",
+        desc_pt: "Área do Treinador de Alien, Virell",
+        desc_en: "Alien Trainer Virell Patch"
+    },
+    "0024": {
+        file: "assets/areas/0024.png",
+        region: "Great Plains",
+        desc_pt: "Área do Arqueólogo Rance",
+        desc_en: "Archaeologist Rance Patch"
+    },
+    "0023": {
+        file: "assets/areas/0023.png",
+        region: "Great Plains",
+        desc_pt: "Área do Rival Ryuma",
+        desc_en: "Rival Ryuma Patch"
+    },
+    "0022": {
+        file: "assets/areas/0022.png",
+        region: "Great Plains",
+        desc_pt: "Goblin Cave",
+        desc_en: "Goblin Cave Area"
+    },
+    "0021": {
+        file: "assets/areas/0021.png",
+        region: "Great Plains",
+        desc_pt: "Área do Coletor de Insetos, Seth",
+        desc_en: "Bug Catcher Seth Patch"
+    },
+    "0020": {
+        file: "assets/areas/0020.png",
+        region: "Great Plains",
+        desc_pt: "Área entre as Cordas",
+        desc_en: "Between Ropes Patch"
+    },
+    "0019": {
+        file: "assets/areas/0019.png",
+        region: "Great Plains",
+        desc_pt: "Área da Torre do Korin",
+        desc_en: "Korin Tower Patch"
+    },
+    "0018": {
+        file: "assets/areas/0018.png",
+        region: "Great Plains",
+        desc_pt: "Área antes da Torre do Korin",
+        desc_en: "Before Korin Tower Patch"
+    },
+    "0017": {
+        file: "assets/areas/0017.png",
+        region: "Great Plains",
+        desc_pt: "Área do Homeless Jess",
+        desc_en: "Homeless Jess Patch"
+    },
+    "0016": {
+        file: "assets/areas/0016.png",
+        region: "Great Plains",
+        desc_pt: "Área de Pesca",
+        desc_en: "Fishing Area"
+    },
+    "0015": {
+        file: "assets/areas/0015.png",
+        region: "Great Plains",
+        desc_pt: "Rocky Cave",
+        desc_en: "Rocky Cave Area"
+    },
+    "0014": {
+        file: "assets/areas/0014.png",
+        region: "Great Plains",
+        desc_pt: "Área antes da Rocky Cave",
+        desc_en: "Before Rocky Cave Patch"
+    },
+    "0013": {
+        file: "assets/areas/0013.png",
+        region: "Great Plains",
+        desc_pt: "Área depois do Climber Cade",
+        desc_en: "After Climber Cade Patch"
+    },
+    "0012": {
+        file: "assets/areas/0012.png",
+        region: "Great Plains",
+        desc_pt: "Área do Homeless Bart",
+        desc_en: "Homeless Bart Patch"
+    },
+    "0011": {
+        file: "assets/areas/0011.png",
+        region: "Great Plains",
+        desc_pt: "Área do lado do centro de cura",
+        desc_en: "Healing Center Side Patch"
+    },
+    "0010": {
+        file: "assets/areas/0010.png",
+        region: "Great Plains",
+        desc_pt: "Área Inicial",
+        desc_en: "Initial Patch"
+    },
     "0009": {
         file: "assets/areas/0009.png",
         region: "Sunburst Forest",
